4793694c7f7fe2a772a0268a4339281409091740
Jane Justiz <janejustiz@gmail.com>
2019-07-30 23:25:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,5 +19,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.title) FROM characters LEFT OUTER JOIN series ON characters.series_id=series.id LEFT OUTER JOIN books ON series.id=books.series_id GROUP BY characters.name ORDER BY COUNT(books.title) DESC"
+  "SELECT characters.name, books.title FROM characters LEFT OUTER JOIN series ON characters.series_id=series.id LEFT OUTER JOIN books ON series.id=books.series_id GROUP BY characters.name"
 end
